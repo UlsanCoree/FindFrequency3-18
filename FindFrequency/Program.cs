5a2506dabf73f5e2707e1d11e52c8efcf5f2bb6a
Kim SeungHyun <korafa@naver.com>
2024-03-18 09:13:38 +0900

    초기 커밋.

---
+++ FindFrequency/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FindFrequency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "사과 바나나 사과 파인애플 블루베리 바나나 사과 참외 수박 파인애플";
+            string[] words = text.Split(' ');
+            var wordFrequency = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                if (wordFrequency.ContainsKey(word))
+                {
+                    wordFrequency[word]++;
+                }
+                else
+                {
+                    wordFrequency[word] = 1;
+                }
+            }
+
+            foreach (var pair in wordFrequency)
+            {
+                Console.WriteLine($"{pair.Key} : {pair.Value}");
+            }
+        }
+    }
+}
+
